ac73b61f933dc6076575715bead7c3c03cf7ccec
dswwfg <dswwfg@snu.ac.kr>
2019-11-20 17:01:11 +0900

    Update return_error_errorJacob.m

--- return_error_errorJacob.m
+++ return_error_errorJacob.m
@@ -1,7 +1,8 @@
-function [error_out, grad_error_out] = return_error_errorJacob(error_intput,parameter)
+function [error_out, grad_error_out] = return_error_errorJacob(error_input,parameter)
 %UNTITLED 이 함수의 요약 설명 위치
 %   자세한 설명 위치
-error = error_intput;
-grad_error_out = jacobian(error_intput', parameter).';
+error_out = error_input;
+grad_error_out = jacobian(error_input', parameter).';
+disp(size(grad_error_out))
 end
 
