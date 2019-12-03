f92c5720ed407010b7cf98e1aaefd986d3314a20
dswwfg <dswwfg@snu.ac.kr>
2019-12-03 14:16:24 +0900

    offset_TH1, TH2, TH3, TH4에 대해 검증완료

--- pos_endEffector.m
+++ pos_endEffector.m
@@ -13,8 +13,7 @@ function output = pos_endEffector(parameter_input, data)
     off_TH2 = 10; off_TH2 = off_TH2*pi/180;
     off_TH3 = 10; off_TH3 = off_TH3*pi/180;
     off_TH4 = 10; off_TH4 = off_TH4*pi/180;
-    
-    %data(i, 1), data(i, 2) 10µµ 
+        
     for i=1:size(data, 1)
         diff_x = 56.03*cos(data(i,1) + off_TH1) + 37.0*sin(data(i,4) + off_TH4)*(cos(data(i,1) + off_TH1)*cos(data(i,2) + off_TH2)*cos(data(i,3) + off_TH3) - 1.0*cos(data(i,1) + off_TH1)*sin(data(i,2) + off_TH2)*sin(data(i,3) + off_TH3)) + 12.94*cos(data(i,1) + off_TH1)*cos(data(i,2) + off_TH2) + 57.5*cos(data(i,1) + off_TH1)*sin(data(i,2) + off_TH2) + 37.0*cos(data(i,4) + off_TH4)*(cos(data(i,1) + off_TH1)*cos(data(i,2) + off_TH2)*sin(data(i,3) + off_TH3) + cos(data(i,1) + off_TH1)*cos(data(i,3) + off_TH3)*sin(data(i,2) + off_TH2)) + 17.25*cos(data(i,1) + off_TH1)*cos(data(i,2) + off_TH2)*sin(data(i,3) + off_TH3) + 17.25*cos(data(i,1) + off_TH1)*cos(data(i,3) + off_TH3)*sin(data(i,2) + off_TH2) + 24.21;
         ref_x  = 56.03*cos(data(i,1) + parameter_input(1)) + 37.0*sin(data(i,4) + parameter_input(4))*(cos(data(i,1) + parameter_input(1))*cos(data(i,2) + parameter_input(2))*cos(data(i,3) + parameter_input(3)) - 1.0*cos(data(i,1) + parameter_input(1))*sin(data(i,2) + parameter_input(2))*sin(data(i,3) + parameter_input(3))) + 12.94*cos(data(i,1) + parameter_input(1))*cos(data(i,2) + parameter_input(2)) + 57.5*cos(data(i,1) + parameter_input(1))*sin(data(i,2) + parameter_input(2)) + 37.0*cos(data(i,4) + parameter_input(4))*(cos(data(i,1) + parameter_input(1))*cos(data(i,2) + parameter_input(2))*sin(data(i,3) + parameter_input(3)) + cos(data(i,1) + parameter_input(1))*cos(data(i,3) + parameter_input(3))*sin(data(i,2) + parameter_input(2))) + 17.25*cos(data(i,1) + parameter_input(1))*cos(data(i,2) + parameter_input(2))*sin(data(i,3) + parameter_input(3)) + 17.25*cos(data(i,1) + parameter_input(1))*cos(data(i,3) + parameter_input(3))*sin(data(i,2) + parameter_input(2)) + 24.21;
