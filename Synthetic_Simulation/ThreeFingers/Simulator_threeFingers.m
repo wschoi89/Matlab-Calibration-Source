735566e28d5906094aaba0df8fb0822668ccb9e7
dswwfg <dswwfg@snu.ac.kr>
2020-01-10 10:40:35 +0900

    각 finger origin 추가

--- Synthetic_Simulation/ThreeFingers/Simulator_threeFingers.m
+++ Synthetic_Simulation/ThreeFingers/Simulator_threeFingers.m
@@ -67,25 +67,34 @@ pos_frame = cell(1, 7, 3);
 % load positions for CAD zig
 load pos_calibration.mat
 
-% plot positions for CAD zig
-color_position = {[0 0 0], [1 0 0], [0 0 1]};
+% color for each finger (thumb, index,and middle finger)
+color_position = {[1 0 0], [0 0.8 0], [0 0 1]};
 figure;
 subplot(1,2,1);
+% plot thumb's origin
+plot3(Origin_thumb(1,4), Origin_thumb(2,4), Origin_thumb(3,4), '-o','MarkerSize',10,'MarkerFaceColor', color_position{1}, 'MarkerEdgeColor', color_position{1})
+hold on
+subplot(1,2,2);
+% plot index and middle finger's origin
+plot3(Origin_index(1,4), Origin_index(2,4), Origin_index(3,4), '-o','MarkerSize',10,'MarkerFaceColor', color_position{2}, 'MarkerEdgeColor', color_position{2})
+hold on
+plot3(Origin_middle(1,4), Origin_middle(2,4), Origin_middle(3,4), '-o','MarkerSize',10,'MarkerFaceColor',color_position{3}, 'MarkerEdgeColor',color_position{3})
+hold on
 for finger=1:num_fingers
     if finger==1
-        
-        plot3(Origin_thumb(1,4), Origin_thumb(2,4), Origin_thumb(3,4), '-o','MarkerSize',8,'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g')
-        hold on
+        subplot(1,2,1);
+        % plot thumb zig positions
         for row=1:size(pos_calibZig{1,finger}, 1)
-            plot3(pos_calibZig{1,finger}(row,1), pos_calibZig{1,finger}(row,2), pos_calibZig{1,finger}(row,3), '-o','MarkerSize',4,'MarkerFaceColor', color_position{finger}, 'MarkerEdgeColor', color_position{finger})
+            plot3(pos_calibZig{1,finger}(row,1), pos_calibZig{1,finger}(row,2), pos_calibZig{1,finger}(row,3), '-o','MarkerSize',8,'MarkerFaceColor', color_position{finger}, 'MarkerEdgeColor', color_position{finger})
             hold on
         end
-        hold on
+        
         
     else
         subplot(1,2,2);
+        % plot index and middle finger's zig positions
         for row=1:size(pos_calibZig{1,finger}, 1)
-            plot3(pos_calibZig{1,finger}(row,1), pos_calibZig{1,finger}(row,2), pos_calibZig{1,finger}(row,3), '-o','MarkerSize',4,'MarkerFaceColor', color_position{finger}, 'MarkerEdgeColor', color_position{finger})
+            plot3(pos_calibZig{1,finger}(row,1), pos_calibZig{1,finger}(row,2), pos_calibZig{1,finger}(row,3), '-o','MarkerSize',8,'MarkerFaceColor', color_position{finger}, 'MarkerEdgeColor', color_position{finger})
             hold on
         end
         view(-180,0)
@@ -93,67 +102,67 @@ for finger=1:num_fingers
 end
 
 % load magnet data from files
-fileName_magneticData = '../DAQ/200102_DAQ_T1_I1_M1.csv';
-for i=1:16
-    fileName_magneticData=strcat('../DAQ/200102_DAQ_T',num2str(i),'_I',num2str(i),'_M',num2str(i),'.csv');
-
-magnetic_data = load(fileName_magneticData);
-
-% convert magnetic data into joint angles
-jointAngle = getJointAngle(magnetic_data);
-
-pos_frame = cell(1, num_joints, num_fingers);
-for finger=1:num_fingers
-    for joint=1:num_joints
-        pos_frame{1,joint,finger} = zeros(size(jointAngle, 1),3);
-    end
-end
-
-
-for finger=1:num_fingers
-    for pos=1:size(jointAngle,1)
-        DH_temp = DH_ref;
-        DH_temp(2,2)=jointAngle(pos,4*(finger-1)+1);
-        DH_temp(3,2)=jointAngle(pos,4*(finger-1)+2);
-        DH_temp(5,2)=jointAngle(pos,4*(finger-1)+3);
-        DH_temp(6,2)=jointAngle(pos,4*(finger-1)+4);
-        DH_temp(2:end,3) = arr_links(:,finger);
-        DH_table(:,:,finger) = DH_temp;
-        
-        for joint=1:num_joints
-            %transformation matrix
-            mat_transform{1, joint, finger}=transform_DH(DH_table(:,:,finger), joint, joint-1);
-    
-            %frame's transform matrix 
-            if joint == 1
-                frame{1,joint,finger} = Origin(:,:,finger)*cell2mat(mat_transform(1,1,finger));
-            else
-                frame{1,joint, finger} = cell2mat(frame(1,joint-1,finger))*cell2mat(mat_transform(1,joint,finger));
-            end
-
-            % frame's origin position
-            pos_frame{1,joint,finger}(pos,:)=[frame{1,joint,finger}(1,4) frame{1,joint,finger}(2,4) frame{1,joint,finger}(3,4)];
-        end
-    end
-end
-
-for finger=1:num_fingers
-    if finger==1
-        subplot(1,2,1);
-        for pos=1:size(jointAngle,1)
-           plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
-           hold on
-%            view([-0.6789*180/pi, 0.5029*180/pi, -0.5350*180/pi])
-        end
-    else
-        subplot(1,2,2);
-        for pos=1:size(jointAngle,1)
-           plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
-           hold on
-        end
-    end
-end
-end
+% fileName_magneticData = '../DAQ/200102_DAQ_T1_I1_M1.csv';
+% for i=1:16
+%     fileName_magneticData=strcat('../DAQ/200102_DAQ_T',num2str(i),'_I',num2str(i),'_M',num2str(i),'.csv');
+% 
+%     magnetic_data = load(fileName_magneticData);
+% 
+%     % convert magnetic data into joint angles
+%     jointAngle = getJointAngle(magnetic_data);
+% 
+%     pos_frame = cell(1, num_joints, num_fingers);
+%     for finger=1:num_fingers
+%         for joint=1:num_joints
+%             pos_frame{1,joint,finger} = zeros(size(jointAngle, 1),3);
+%         end
+%     end
+% 
+% 
+%     for finger=1:num_fingers
+%         for pos=1:size(jointAngle,1)
+%             DH_temp = DH_ref;
+%             DH_temp(2,2)=jointAngle(pos,4*(finger-1)+1);
+%             DH_temp(3,2)=jointAngle(pos,4*(finger-1)+2);
+%             DH_temp(5,2)=jointAngle(pos,4*(finger-1)+3);
+%             DH_temp(6,2)=jointAngle(pos,4*(finger-1)+4);
+%             DH_temp(2:end,3) = arr_links(:,finger);
+%             DH_table(:,:,finger) = DH_temp;
+% 
+%             for joint=1:num_joints
+%                 %transformation matrix
+%                 mat_transform{1, joint, finger}=transform_DH(DH_table(:,:,finger), joint, joint-1);
+% 
+%                 %frame's transform matrix 
+%                 if joint == 1
+%                     frame{1,joint,finger} = Origin(:,:,finger)*cell2mat(mat_transform(1,1,finger));
+%                 else
+%                     frame{1,joint, finger} = cell2mat(frame(1,joint-1,finger))*cell2mat(mat_transform(1,joint,finger));
+%                 end
+% 
+%                 % frame's origin position
+%                 pos_frame{1,joint,finger}(pos,:)=[frame{1,joint,finger}(1,4) frame{1,joint,finger}(2,4) frame{1,joint,finger}(3,4)];
+%             end
+%         end
+%     end
+% 
+%     for finger=1:num_fingers
+%         if finger==1
+%             subplot(1,2,1);
+%             for pos=1:size(jointAngle,1)
+%                plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
+%                hold on
+%                view([-0.6789*180/pi, 0.5029*180/pi, -0.5350*180/pi])
+%             end
+%         else
+%             subplot(1,2,2);
+%             for pos=1:size(jointAngle,1)
+%                plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
+%                hold on
+%             end
+%         end
+%     end
+% end
 
 
 
