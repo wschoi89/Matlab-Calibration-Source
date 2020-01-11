883a8801ba709357897563a4e6fa0f06b510370f
dswwfg <dswwfg@snu.ac.kr>
2020-01-11 23:12:27 +0900

    CAD zig position과 계산된 end-effector position 저장

--- Synthetic_Simulation/ThreeFingers/plotThreeFingers_thumb_seperately.m
+++ Synthetic_Simulation/ThreeFingers/plotThreeFingers_thumb_seperately.m
@@ -8,6 +8,10 @@ arr_links = loadLinkLength();
 num_joints = 7;
 num_param_per_joint = 4; % DH parameter
 num_fingers = 3;
+num_zigThumbPos = 21;
+num_zigIndexPos = 16;
+num_zigMiddlePos = 16;
+num_data = 100;
 
 % set each finger's origin position
 Origin_index = eye(4);
@@ -32,8 +36,11 @@ DH_table = zeros(num_joints, num_param_per_joint, num_fingers);
 mat_transform = cell(1, 7, 3);
 frame = cell(1, 7, 3);
 pos_frame = cell(1, 7, 3);
+pos_endEffector = cell(1,3);
 
-
+pos_endEffector{1,1} = zeros(num_data,3,num_zigThumbPos);
+pos_endEffector{1,2} = zeros(num_data,3,num_zigIndexPos);
+pos_endEffector{1,3} = zeros(num_data,3,num_zigMiddlePos);
 
 % for finger=1:num_fingers
 % %     angle_TH1(i) = -30+60*rand(1);
@@ -68,7 +75,7 @@ pos_frame = cell(1, 7, 3);
 load pos_calibration_thumb_seperately.mat
 
 % color for each finger (thumb, index,and middle finger)
-color_position = {[1 0 0], [0 0.8 0], [0 0 1]};
+color_position = {[1 0 0], [0 0.5 0], [0 0 1]};
 figure;
 subplot(1,2,1);
 % plot thumb's origin
@@ -122,70 +129,115 @@ hold on
 plot3(Origin_middle_projToPlane(1,4), Origin_middle_projToPlane(2,4), Origin_middle_projToPlane(3,4), '-o','MarkerSize',10,'MarkerFaceColor',color_position{3}, 'MarkerEdgeColor',color_position{3})
 hold on
 
-%% hi 
+%% draw line to connect zig positions
+subplot(1,2,1);
+%thumb
+for row=1:size(pos_calibZig{1,1}, 1) 
+    plot3([Origin_thumb_projToPlane(1,4) pos_calibZig{1,1}(row,1)], [Origin_thumb_projToPlane(2,4) pos_calibZig{1,1}(row,2)], [Origin_thumb_projToPlane(3,4) pos_calibZig{1,1}(row,3)], 'Color', [1 0 0])
+    hold on
+end
 
-% load magnet data from files
-% fileName_magneticData = '../DAQ/200102_DAQ_T1_I1_M1.csv';
-% for i=1:16
-%     fileName_magneticData=strcat('../DAQ/200102_DAQ_T',num2str(i),'_I',num2str(i),'_M',num2str(i),'.csv');
-% 
-%     magnetic_data = load(fileName_magneticData);
-% 
-%     % convert magnetic data into joint angles
-%     jointAngle = getJointAngle(magnetic_data);
-% 
-%     pos_frame = cell(1, num_joints, num_fingers);
-%     for finger=1:num_fingers
-%         for joint=1:num_joints
-%             pos_frame{1,joint,finger} = zeros(size(jointAngle, 1),3);
-%         end
-%     end
-% 
-% 
-%     for finger=1:num_fingers
-%         for pos=1:size(jointAngle,1)
-%             DH_temp = DH_ref;
-%             DH_temp(2,2)=jointAngle(pos,4*(finger-1)+1);
-%             DH_temp(3,2)=jointAngle(pos,4*(finger-1)+2);
-%             DH_temp(5,2)=jointAngle(pos,4*(finger-1)+3);
-%             DH_temp(6,2)=jointAngle(pos,4*(finger-1)+4);
-%             DH_temp(2:end,3) = arr_links(:,finger);
-%             DH_table(:,:,finger) = DH_temp;
-% 
-%             for joint=1:num_joints
-%                 %transformation matrix
-%                 mat_transform{1, joint, finger}=transform_DH(DH_table(:,:,finger), joint, joint-1);
-% 
-%                 %frame's transform matrix 
-%                 if joint == 1
-%                     frame{1,joint,finger} = Origin(:,:,finger)*cell2mat(mat_transform(1,1,finger));
-%                 else
-%                     frame{1,joint, finger} = cell2mat(frame(1,joint-1,finger))*cell2mat(mat_transform(1,joint,finger));
-%                 end
-% 
-%                 % frame's origin position
-%                 pos_frame{1,joint,finger}(pos,:)=[frame{1,joint,finger}(1,4) frame{1,joint,finger}(2,4) frame{1,joint,finger}(3,4)];
-%             end
-%         end
-%     end
-% 
-%     for finger=1:num_fingers
-%         if finger==1
-%             subplot(1,2,1);
-%             for pos=1:size(jointAngle,1)
-%                plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
-%                hold on
-%                view([-0.6789*180/pi, 0.5029*180/pi, -0.5350*180/pi])
-%             end
-%         else
-%             subplot(1,2,2);
-%             for pos=1:size(jointAngle,1)
-%                plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-*') 
-%                hold on
-%             end
-%         end
-%     end
-% end
+subplot(1,2,2);
+% index finger
+for row=1:size(pos_calibZig{1,2}, 1) 
+    plot3([Origin_index_projToPlane(1,4) pos_calibZig{1,2}(row,1)], [Origin_index_projToPlane(2,4) pos_calibZig{1,2}(row,2)], [Origin_index_projToPlane(3,4) pos_calibZig{1,2}(row,3)],'Color',[0 0.5 0])
+    hold on
+end
+
+% middle finger
+for row=1:size(pos_calibZig{1,3}, 1) 
+    plot3([Origin_middle_projToPlane(1,4) pos_calibZig{1,3}(row,1)], [Origin_middle_projToPlane(2,4) pos_calibZig{1,3}(row,2)], [Origin_middle_projToPlane(3,4) pos_calibZig{1,3}(row,3)],'Color',[0 0 1])
+    hold on
+end
+
+
+%%  load magnet data from files
+
+fileName_magneticData = '../DAQ/200102_DAQ_T1_I1_M1.csv';
+for i=1:21
+    
+    fileName_magneticData=strcat('../DAQ/200102_DAQ_T',num2str(i),'_I',num2str(i),'_M',num2str(i),'.csv');
+    magnetic_data = load(fileName_magneticData);
+
+    % convert magnetic data into joint angles
+    jointAngle = getJointAngle(magnetic_data);
+
+    pos_frame = cell(1, num_joints, num_fingers);
+    for finger=1:num_fingers
+        for joint=1:num_joints
+            pos_frame{1,joint,finger} = zeros(size(jointAngle, 1),3);
+        end
+    end
+
+
+    for finger=1:num_fingers
+        for pos=1:size(jointAngle,1)
+            DH_temp = DH_ref;
+            DH_temp(2,2)=jointAngle(pos,4*(finger-1)+1);
+            DH_temp(3,2)=jointAngle(pos,4*(finger-1)+2);
+            DH_temp(5,2)=jointAngle(pos,4*(finger-1)+3);
+            DH_temp(6,2)=jointAngle(pos,4*(finger-1)+4);
+            DH_temp(2:end,3) = arr_links(:,finger);
+            DH_table(:,:,finger) = DH_temp;
+
+            for joint=1:num_joints
+                %transformation matrix
+                mat_transform{1, joint, finger}=transform_DH(DH_table(:,:,finger), joint, joint-1);
+
+                %frame's transform matrix 
+                if joint == 1
+                    frame{1,joint,finger} = Origin(:,:,finger)*cell2mat(mat_transform(1,1,finger));
+                else
+                    frame{1,joint, finger} = cell2mat(frame(1,joint-1,finger))*cell2mat(mat_transform(1,joint,finger));
+                end
+
+                % frame's origin position
+                pos_frame{1,joint,finger}(pos,:)=[frame{1,joint,finger}(1,4) frame{1,joint,finger}(2,4) frame{1,joint,finger}(3,4)];
+            end
+        end
+    end
+
+    % plot calculated end-effector positions
+    for finger=1:num_fingers 
+        if finger==1
+            subplot(1,2,1);
+            for pos=1:size(jointAngle,1)
+               plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-o', 'Color', [0.5 0 0])
+               hold on
+               pos_endEffector{1,finger}(pos,:,i) = pos_frame{1,7,finger}(pos,:);
+            end
+        elseif finger == 2 && i<17
+            subplot(1,2,2);
+            for pos=1:size(jointAngle,1)
+               plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-o', 'Color', [0 0.2 0]) 
+               hold on
+               pos_endEffector{1,finger}(pos,:,i) = pos_frame{1,7,finger}(pos,:);
+            end
+        elseif finger==3 && i<17
+            subplot(1,2,2);
+            for pos=1:size(jointAngle,1)
+               plot3(pos_frame{1,7,finger}(pos,1),pos_frame{1,7,finger}(pos,2),pos_frame{1,7,finger}(pos,3),'-o','Color',[0 0 1]) 
+               hold on
+               pos_endEffector{1,finger}(pos,:,i) = pos_frame{1,7,finger}(pos,:);
+            end
+        end
+    end
+end
+
+% save data
+save('training_test_data.mat', 'pos_calibZig', 'pos_endEffector')
+
+% thumb 21개 position(x,y,z) 100 개, 21*3*100
+% CAD_thumb : 21개 x,y,z 21*3
+% index 16개 position(x,y,z) 100 개, 16*3*100
+% CAD_index : 16개 x,y,z, 16*3
+% middle 16개 position(x,y,z) 100개, 16*3*100
+% CAD_middle : 16개 x,y,z, 16*3
+
+
+
+
+
 
 
 
