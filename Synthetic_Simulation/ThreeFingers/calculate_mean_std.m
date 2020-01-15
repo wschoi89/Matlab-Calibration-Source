01129caccc582c87e6b985271c4b0f7277a985e8
dswwfg <dswwfg@snu.ac.kr>
2020-01-15 15:54:48 +0900

    Create calculate_mean_std.m

---
+++ Synthetic_Simulation/ThreeFingers/calculate_mean_std.m
@@ -0,0 +1,21 @@
+mean(arr_mean_dist_noCalib{1,1}(1,4,:))
+mean(arr_mean_dist_noCalib{1,2}(1,4,:))
+mean(arr_mean_dist_noCalib{1,3}(1,4,:))
+
+clear std
+std(arr_mean_dist_noCalib{1,1}(1,4,:))
+clear std
+std(arr_mean_dist_noCalib{1,2}(1,4,:))
+clear std
+std(arr_mean_dist_noCalib{1,3}(1,4,:))
+
+mean(arr_mean_dist_Calib{1,1}(1,4,:))
+mean(arr_mean_dist_Calib{1,2}(1,4,:))
+mean(arr_mean_dist_Calib{1,3}(1,4,:))
+
+clear std
+std(arr_mean_dist_Calib{1,1}(1,4,:))
+clear std
+std(arr_mean_dist_Calib{1,2}(1,4,:))
+clear std
+std(arr_mean_dist_Calib{1,3}(1,4,:))
