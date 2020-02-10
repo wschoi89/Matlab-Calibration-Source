c13a9438e1e5200891d672501bf234f66bf99739
dswwfg <dswwfg@snu.ac.kr>
2020-02-10 11:16:06 +0900

    Update comp_transform_cad_matlab.m

--- Synthetic_Simulation/comp_transform_cad_matlab.m
+++ Synthetic_Simulation/comp_transform_cad_matlab.m
@@ -1,29 +1,30 @@
 
+%% calculate transformation matrix of Thumb origin w.r.t Index origin coordinate.
+
 transform_thumb = zeros(4,4);
 transform_index = zeros(4,4);
 
 % rotation matrix from solidworks API, row-major
-rot_thumb = [-0.831073087909498 -0.168555665338342 -0.530006141695472;
-    -0.534952315874943 0.502936510347408 0.678882085711213;
-    0.15213001772368 0.847728644226394 -0.508146242203436;];
+rot_thumb = [-0.844687214233004 -5.46702184259277E-02 -0.53246096319688;
+    -0.5352337723626 9.61601150699119E-02 0.839212750851798;
+    5.3215630963567E-03 0.993863370633511 -0.110486566962726;];
 
-transl_thumb = [289.585110558779, 22.6140624395133, 493.939187432284];
+transl_thumb = [-54.9179252969987 -78.7790464130597 114.051754006815];
 
 transform_thumb(1:3,1:3) = rot_thumb';
 transform_thumb(:,4) = [transl_thumb'; 1];
 
 % rotation matrix from solidworks API, row-major
-rot_index = [-9.36683924326055E-13 -2.80351095930295E-29 -1;
--2.99301705355953E-17 1 0;
-1 2.99301705355953E-17 -9.36683924326055E-13;];
+rot_index = [-2.24451969705654E-14 0.500000000000002 -0.866025403784437;
+            1.34286534719403E-14 0.866025403784437 0.500000000000002;
+            1 -4.06956560035623E-16 -2.61524375054253E-14;];
 
-transl_index = [313.937353494065, 51.6545882406963, 405.189551659303];
+transl_index = [-29.5553631585038 -8.22350590165543 52.2435300380096];
 
 transform_index(1:3,1:3) = rot_index';
 transform_index(:,4) = [transl_index'; 1];
-
     
 transform_thumb_wrt_index = inv(transform_index)*transform_thumb
 
-transform_thumb_matlabCode = eye(4,4)*transl(-88.75,-29.04,-24.35)*trotz(24*pi/180)*trotx(-75*pi/180)*troty(54*pi/180)*trotz(45*pi/180)
+% transform_thumb_matlabCode = eye(4,4)*transl(-88.75,-29.04,-24.35)*trotz(24*pi/180)*trotx(-75*pi/180)*troty(54*pi/180)*trotz(45*pi/180)
 
