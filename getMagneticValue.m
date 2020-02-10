8d92d1cead35ab392b8157e8dde6a16cfca28010
dswwfg <dswwfg@snu.ac.kr>
2020-02-10 11:02:51 +0900

    added comments

--- getMagneticValue.m
+++ getMagneticValue.m
@@ -1,13 +1,22 @@
 function [bx, by, bz] = getMagneticValue(sensor1)
+    
+    % input : 7 bytes
+    % 1st byte = Bx(4~11)
+    % 2nd byte = By(4~11)
+    % 3rd byte = Bz(4~11)
+    % 4th byte = temperature
+    % 5th byte = Bx(0~3), By(0~3)
+    % 6th byte = Reserve(3), Pd(1), Bz(0~3)
+    %% bx 
     upper = sensor1(1);
-    upper_12 = bitshift(upper, 4);
+    upper_12 = bitshift(upper, 4);% ¿ÞÂÊÀ¸·Î 4Ä­ bitshift, 12bitµÊ
     lower = sensor1(5);
-    lower_4 = bitshift(lower, -4);
+    lower_4 = bitshift(lower, -4);% ¿À¸¥ÂÊÀ¸·Î 4Ä­ bitshift 
 
     bx = bitor(upper_12, lower_4);
-    bx_bin = dec2bin(bx, 12);
+    bx_bin = dec2bin(bx, 12);% convert decimal to binary
 
-    sign = bitand(bx, bin2dec('1000 00000000'));
+    sign = bitand(bx, bin2dec('1000 00000000')); % Bit-wise AND
     adcVal = bitand(bx, bin2dec('0111 11111111'));
 
     unit = 0.098;
