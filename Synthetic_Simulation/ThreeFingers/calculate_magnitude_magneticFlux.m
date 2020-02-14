f77256ca0bd32986a1efc7cce5e575badd099661
dswwfg <dswwfg@snu.ac.kr>
2020-02-14 11:43:25 +0900

    Update calculate_magnitude_magneticFlux.m

--- Synthetic_Simulation/ThreeFingers/calculate_magnitude_magneticFlux.m
+++ Synthetic_Simulation/ThreeFingers/calculate_magnitude_magneticFlux.m
@@ -1,11 +1,20 @@
 
-mag_vec = zeros(num_zigIndexPos,6);
-for n_pos=1:num_zigThumbPos
+num_zigPos = 3;
+mag_vec = zeros(num_zigPos(1),18);
+for n_pos=1:num_zigPos(1)
     
-    fileName_magneticData=strcat('../DAQ/device3/200114_DAQ_T',num2str(n_pos),'_I',num2str(n_pos),'_M',num2str(n_pos),'.csv');
+    fileName_magneticData=strcat('DAQ/device2_/device2__DAQ_T',num2str(n_pos),'_I',num2str(n_pos),'_M',num2str(n_pos),'_training','.csv');
     magnetic_data = load(fileName_magneticData);
-    mean_magnet = mean(magnetic_data);
-    for i=1:6
-        mag_vec(n_pos, i) = sqrt(mean_magnet(3*(i-1)+1)^2+mean_magnet(3*(i-1)+2)^2+mean_magnet(3*i)^2);
-    end
+    mean_magnet(n_pos, :) = mean(magnetic_data);
+%     for i=1:6
+%         mag_vec(n_pos, i) = sqrt(mean_magnet(3*(i-1)+1)^2+mean_magnet(3*(i-1)+2)^2+mean_magnet(3*i)^2);
+%     end
 end
+
+mean(mag_vec)
+clear std
+std(mag_vec)
+
+
+
+
