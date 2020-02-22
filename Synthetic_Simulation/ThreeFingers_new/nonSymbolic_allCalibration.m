eefe62188efc16913b74bbcfd2839ff453bc19e5
dswwfg <dswwfg@snu.ac.kr>
2020-02-22 21:29:33 +0900

    sample 변수 추가

--- Synthetic_Simulation/ThreeFingers_new/nonSymbolic_allCalibration.m
+++ Synthetic_Simulation/ThreeFingers_new/nonSymbolic_allCalibration.m
@@ -3,7 +3,7 @@
 % options = optimoptions(@lsqnonlin,'Algorithm', 'trust-region-reflective','Display', 'iter', 'MaxFunctionEvaluations', 20000, 'MaxIterations', 20000, 'FiniteDifferenceType', 'central','PlotFcn','optimplotx', 'DiffMaxChange', 0.1);
 
 disp('optimization start')
-options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 100000, 'MaxIterations', 100000, 'initDamping', 10,'StepTolerance', 1e-10);
+options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 50000, 'MaxIterations', 50000, 'initDamping', 10,'StepTolerance', 1e-10);
 
 % load data
 load training_test_data.mat
@@ -24,7 +24,9 @@ for iter=1:num_fingers
         
 
 
-sample = 100;
+if ~exist('num_samples','var')
+    num_samples = 100;
+end
 
 
 % thumb
@@ -32,16 +34,16 @@ if strcmp(finger, 'thumb')
     training_thumb = pos_endEffector_noCalib{1,1};
     test_thumb = pos_calibZig{1,1};
     [row,col,page] = size(training_thumb);
-    data_training=zeros(num_zigPos(1)*sample,3);
-    data_test = zeros(num_zigPos(1)*sample,3); % cad data
-    data_jointAngle = zeros(num_zigPos(1)*sample,4);
+    data_training=zeros(num_zigPos(1)*num_samples,3);
+    data_test = zeros(num_zigPos(1)*num_samples,3); % cad data
+    data_jointAngle = zeros(num_zigPos(1)*num_samples,4);
     data_jointAngle_thumb = data_jointAngle(:,1:4);    
     
     for p=1:page
         data_training(row*(p-1)+1:row*p,:) = training_thumb(:,:,p);
         data_jointAngle_thumb(row*(p-1)+1:row*p,:) = arr_jointAngles(:,1:4,p);
         for r=1:row
-            data_test(100*(p-1)+r,:) = test_thumb(p,1:3);
+            data_test(num_samples*(p-1)+r,:) = test_thumb(p,1:3);
 
         end
     end
@@ -52,16 +54,16 @@ elseif strcmp(finger, 'index')
     test_index = pos_calibZig{1,2};
     training_index = pos_endEffector_noCalib{1,2};
     [row,col,page] = size(training_index);
-    data_training=zeros(num_zigPos(2)*sample,3);
-    data_test = zeros(num_zigPos(2)*sample,3);
-    data_jointAngle_indexMiddle = zeros(num_zigPos(2)*sample, 8);
+    data_training=zeros(num_zigPos(2)*num_samples,3);
+    data_test = zeros(num_zigPos(2)*num_samples,3);
+    data_jointAngle_indexMiddle = zeros(num_zigPos(2)*num_samples, 8);
     data_jointAngle_index = data_jointAngle_indexMiddle(:,1:4);
 
     for p=1:page
         data_training(row*(p-1)+1:row*p,:) = training_index(:,:,p);
         data_jointAngle_index(row*(p-1)+1:row*p,:) = arr_jointAngles(:,5:8,p);
         for r=1:row
-            data_test(100*(p-1)+r,:) = test_index(p,1:3);
+            data_test(num_samples*(p-1)+r,:) = test_index(p,1:3);
 
         end
     end
@@ -72,16 +74,16 @@ elseif strcmp(finger, 'middle')
     test_middle = pos_calibZig{1,3};
     training_middle = pos_endEffector_noCalib{1,3};
     [row,col,page] = size(training_middle);
-    data_training=zeros(num_zigPos(3)*sample,3);
-    data_test = zeros(num_zigPos(3)*sample,3);
-    data_jointAngle_indexMiddle = zeros(num_zigPos(3)*sample, 8);
+    data_training=zeros(num_zigPos(3)*num_samples,3);
+    data_test = zeros(num_zigPos(3)*num_samples,3);
+    data_jointAngle_indexMiddle = zeros(num_zigPos(3)*num_samples, 8);
     data_jointAngle_middle = data_jointAngle_indexMiddle(:,5:8);
 
     for p=1:page
         data_training(row*(p-1)+1:row*p,:) = training_middle(:,:,p);
         data_jointAngle_middle(row*(p-1)+1:row*p,:) = arr_jointAngles(:,9:12,p);
         for r=1:row
-            data_test(100*(p-1)+r,:) = test_middle(p,1:3);
+            data_test(num_samples*(p-1)+r,:) = test_middle(p,1:3);
 
         end
     end
@@ -104,7 +106,10 @@ list_optParam = zeros(num_iteration, num_optParam);
 
 for page=1:num_iteration
         
-    optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration,zeros(1,num_optParam),[],[],options, data, finger);
+    lb = [-0.5;-0.5;-0.5;-0.5;-5;-5;-5;-5;-5;-5;-5;-5;-5;-5;-0.5;-0.5;-0.5;-0.5;-0.5;-0.5;];
+    ub = [0.5;0.5;0.5;0.5;5;5;5;5;5;5;5;5;5;5;0.5;0.5;0.5;0.5;0.5;0.5;];
+%     optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration,zeros(1,num_optParam),[],[],options, data, finger);
+    optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration,zeros(1,num_optParam),lb,ub,options, data, finger);
     list_optParam(page, :) = optimized_parameter;
    
 end
