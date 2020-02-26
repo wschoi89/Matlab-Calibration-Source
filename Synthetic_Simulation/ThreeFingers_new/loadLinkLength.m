75681ee28a7e90a6673c537023485f5eec30ec6e
dswwfg <dswwfg@snu.ac.kr>
2020-02-26 14:41:16 +0900

    링크길이 변경

--- Synthetic_Simulation/ThreeFingers_new/loadLinkLength.m
+++ Synthetic_Simulation/ThreeFingers_new/loadLinkLength.m
@@ -8,20 +8,20 @@ function linkLength = loadLinkLength()
     middle = 3;
     %thumb links(7 links for end-effector position)
     linkLength(1, thumb) = 24.21;
-    linkLength(2, thumb) = 54.80;
-    linkLength(3, thumb) = 17.43;
-    linkLength(4, thumb) = 57.51;
-    linkLength(5, thumb) = 14.07;
-    linkLength(6, thumb) = 35.72;
+    linkLength(2, thumb) = 56.03;
+    linkLength(3, thumb) = 12.94;
+    linkLength(4, thumb) = 57.50;
+    linkLength(5, thumb) = 19.50;
+    linkLength(6, thumb) = 24.90;
     
      
 %     %index links
-    linkLength(1, index) = 24.2;
-    linkLength(2, index) = 54.80;
-    linkLength(3, index) = 17.43;
-    linkLength(4, index) = 47.51;
-    linkLength(5, index) = 14.07;
-    linkLength(6, index) = 35.72;
+    linkLength(1, index) = linkLength(1, thumb);
+    linkLength(2, index) = linkLength(2, thumb);
+    linkLength(3, index) = linkLength(3, thumb);
+    linkLength(4, index) = linkLength(4, thumb)-10;
+    linkLength(5, index) = linkLength(5, thumb);
+    linkLength(6, index) = linkLength(6, thumb);
     
 
      
