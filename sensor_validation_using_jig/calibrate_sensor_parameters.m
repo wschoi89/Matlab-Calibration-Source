992eed63ce48418860c404ff315ec8ae09a80589
dswwfg <dswwfg@snu.ac.kr>
2020-03-28 17:16:40 +0900

    Update calibrate_sensor_parameters.m

--- sensor_validation_using_jig/calibrate_sensor_parameters.m
+++ sensor_validation_using_jig/calibrate_sensor_parameters.m
@@ -1,35 +1,53 @@
+close all
+options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 100000, 'MaxIterations', 100000, 'initDamping', 10,'StepTolerance', 1e-10);
 
-% options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 100000, 'MaxIterations', 100000, 'initDamping', 10,'StepTolerance', 1e-10);
-% 
-% data = [arr_mean arr_angles_reference'];
-% parameter_init = [0.01 0.01 0.01 0.01 0.01];
-% optimized_sensor_param=lsqnonlin(@optimize_sensor, parameter_init, [],[],options, data);
-% 
-% offset_y = optimized_sensor_param(1);
-% offset_z = optimized_sensor_param(2);
-% 
-% 
-% amp_y = optimized_sensor_param(3);
-% amp_z = optimized_sensor_param(4);
-% shift = optimized_sensor_param(5);
+data = [arr_mean_normalized arr_angles_reference'];
+parameter_init = [0.01 0.01 0.01 0.01 0.01];
+disp('Optimization start!');
+optimized_sensor_param=lsqnonlin(@optimize_sensor, parameter_init, [],[],options, data);
+
+offset_y = optimized_sensor_param(1);
+offset_z = optimized_sensor_param(2);
+
+
+amp_y = optimized_sensor_param(3);
+amp_z = optimized_sensor_param(4);
+shift = optimized_sensor_param(5);
+
+arr_mean_optimzed = [arr_mean_normalized(:,1) (arr_mean_normalized(:,2)-offset_y)./amp_y (arr_mean_normalized(:,3)-offset_z)./amp_z];
 
 for i=1:size(data,1)
-       %By, Bz
-       up = ((data(i,3)-offset_z)./amp_z - (data(i,2)-offset_y)./amp_y*sin(-shift))/cos(-shift);
-       down = (data(i,2)-offset_y)./amp_y;
-        diff_angle = atan2(up, down)*180/pi -atan2(1,0)*180/pi;
-        
     
-    while(diff_angle < 0)
-        diff_angle=diff_angle + 360;
-    end
-       
-    if i==1
-        diff_angle = diff_angle - 360;
-    end
+    %By, Bz
+    up = ((data(i,3)-offset_z)./amp_z - (data(i,2)-offset_y)./amp_y*sin(-shift))/cos(-shift);
+%     up = ((data(i,3)-offset_z)./amp_z - (data(i,2)-offset_y)./amp_y);
+    down = (data(i,2)-offset_y)./amp_y;
+    diff_angle = atan2(up, down)*180/pi -atan2(1,0)*180/pi;
         
-    arr_angle_calculated(i,1) = diff_angle;
-   ref_angle = data(i,4);
+    arr_angles_optimized(i,1) = diff_angle;
+    ref_angle = data(i,4);
    
-   output(i,1) = diff_angle - ref_angle;
+    diff(i,1) = diff_angle - ref_angle; % differences
 end
+
+
+subplot(1,3,1);
+xlabel('refefence angle(degree)');
+ylabel('optimized angle(degree)');
+plot(arr_angles_reference, arr_mean_optimzed)
+xlabel('refefence angle(degree)');
+ylabel('magnetic flux (mT)')
+legend('Bx', 'By', 'Bz')
+subplot(1,3,2);
+plot(arr_angles_reference, arr_angles_optimized)
+xlabel('refefence angle(degree)');
+ylabel('optimized angle(degree)');
+subplot(1,3,3);
+plot(arr_angles_reference, diff);
+xlabel('refefence angle(degree)');
+ylabel('angle error(degree)');
+title(strcat('Angle error : ', num2str(mean(diff)), ' \pm',num2str(std(diff)),' \circ'));
+sgtitle('After optimization')
+set(gcf, 'position', [100,100,1000,500])
+set(gcf, 'position', [100,100,1000,400])
+saveas(gcf, 'test.jpeg')
