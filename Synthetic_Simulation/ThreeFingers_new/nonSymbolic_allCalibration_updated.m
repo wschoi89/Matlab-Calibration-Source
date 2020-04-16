f3ba6836710db6c9d5b76f088b4f998086450236
dswwfg <dswwfg@snu.ac.kr>
2020-04-16 11:16:14 +0900

    optimization parameter의 상, 하한 범위 추가

--- Synthetic_Simulation/ThreeFingers_new/nonSymbolic_allCalibration_updated.m
+++ Synthetic_Simulation/ThreeFingers_new/nonSymbolic_allCalibration_updated.m
@@ -3,8 +3,8 @@
 % options = optimoptions(@lsqnonlin,'Algorithm', 'trust-region-reflective','Display', 'iter', 'MaxFunctionEvaluations', 20000, 'MaxIterations', 20000, 'FiniteDifferenceType', 'central','PlotFcn','optimplotx', 'DiffMaxChange', 0.1);
 
 disp('optimization start')
-options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 200000, 'MaxIterations', 200000, 'initDamping', 10,'StepTolerance', 1e-10);
-%  options = optimoptions(@lsqnonlin,'Algorithm', 'trust-region-reflective','Display', 'iter', 'MaxFunctionEvaluations', 200000, 'MaxIterations', 200000, 'initDamping', 10,'StepTolerance', 1e-10);
+% options = optimoptions(@lsqnonlin,'Algorithm', 'levenberg-marquardt','Display', 'iter', 'MaxFunctionEvaluations', 200000, 'MaxIterations', 200000, 'initDamping', 10,'StepTolerance', 1e-10);
+ options = optimoptions(@lsqnonlin,'Algorithm', 'trust-region-reflective','Display', 'iter', 'MaxFunctionEvaluations', 200000, 'MaxIterations', 200000, 'initDamping', 10,'StepTolerance', 1e-10);
 
 % load data
 load training_test_data.mat
@@ -100,8 +100,12 @@ for iter=3:3
 
 
     for page=1:num_iteration
-        %optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration_updated,ones(1,num_optParam),[],[],options, data, finger);
-        optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration_updated,list_optParam_init,[],[],options, data, finger);
+        
+        lb = [-5;-5;-5;-5;-5;-5;-5;-5;-5;-5; -0.5;-0.5;-0.5;-0.5;-0.5;-0.5; -2;-2;-2;-2;-2;-2; -2;-2;-2;-2;-2;-2; -0.5;-0.5;-0.5;-0.5; -0.5;-0.5;-0.5;-0.5;];
+        ub = [ 5; 5; 5; 5; 5; 5; 5; 5; 5; 5;  0.5; 0.5; 0.5; 0.5; 0.5; 0.5;  2; 2; 2; 2; 2; 2;  2; 2; 2; 2; 2; 2;  0.5; 0.5; 0.5; 0.5;  0.5; 0.5; 0.5; 0.5;];
+        
+%         optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration_updated,list_optParam_init,[],[],options, data, finger);
+        optimized_parameter=lsqnonlin (@pos_endEffector_allCalibration_updated,list_optParam_init,lb,ub,options, data, finger);
         list_optParam(page, :) = optimized_parameter;
 
     end
