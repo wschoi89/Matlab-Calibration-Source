f6904d265e34013724cb2f2d2a8b3495d7e32200
dswwfg <dswwfg@snu.ac.kr>
2020-04-23 13:47:13 +0900

    함수 parameter 변수명 변경

--- Synthetic_Simulation/ThreeFingers_new_200408/getMagneticValue.m
+++ Synthetic_Simulation/ThreeFingers_new_200408/getMagneticValue.m
@@ -1,4 +1,4 @@
-function [bx, by, bz] = getMagneticValue(sensor1)
+function [bx, by, bz] = getMagneticValue(sensor_in)
     
     % input : 7 bytes
     % 1st byte = Bx(4~11)
@@ -8,9 +8,9 @@ function [bx, by, bz] = getMagneticValue(sensor1)
     % 5th byte = Bx(0~3), By(0~3)
     % 6th byte = Reserve(3), Pd(1), Bz(0~3)
     %% bx 
-    upper = sensor1(1);
+    upper = sensor_in(1);
     upper_12 = bitshift(upper, 4);% ¿ÞÂÊÀ¸·Î 4Ä­ bitshift, 12bitµÊ
-    lower = sensor1(5);
+    lower = sensor_in(5);
     lower_4 = bitshift(lower, -4);% ¿À¸¥ÂÊÀ¸·Î 4Ä­ bitshift 
 
     bx = bitor(upper_12, lower_4);
@@ -26,9 +26,9 @@ function [bx, by, bz] = getMagneticValue(sensor1)
 
     val_bx = adcVal*unit;
     %% sensor 1 by
-    upper = sensor1(2);
+    upper = sensor_in(2);
     upper_12 = bitshift(upper, 4);
-    lower = sensor1(5);
+    lower = sensor_in(5);
     lower_4 = bitand(lower, bin2dec('00001111'));
 
     by = bitor(upper_12, lower_4);
@@ -45,9 +45,9 @@ function [bx, by, bz] = getMagneticValue(sensor1)
 
     %% sensor 1 bz
 
-    upper = sensor1(3);
+    upper = sensor_in(3);
     upper_12 = bitshift(upper, 4);
-    lower = sensor1(6);
+    lower = sensor_in(6);
     lower_4 = bitand(lower, bin2dec('00001111'));
 
     by = bitor(upper_12, lower_4);
